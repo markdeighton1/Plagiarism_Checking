7a26bd5e505e0ecdad49b8bfe81373c168cef648
Exile <Exile@10.34.113.89>
2018-05-03 16:20:15 +0100

    put the test of opening files in main, doesn't work, need to change file location

--- Plagiarism_Checking/src/Main.java
+++ Plagiarism_Checking/src/Main.java
@@ -1,4 +1,30 @@
-
-public class Main {
-	
+import java.io.*;
+public class Main 
+{
+	public static void main(String[] args)
+	{
+String fileName = "test1.txt";
+		
+		String line = null;
+		
+		try {
+			FileReader fileReader = new FileReader(fileName);
+			
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			
+			while((line = bufferedReader.readLine()) != null) 
+			{
+				System.out.println(line);
+			}
+			
+			bufferedReader.close();
+		}
+		catch(FileNotFoundException ex)
+		{
+			System.out.println("Unable to open file '" + fileName + "'");
+		}
+		catch(IOException ex) {
+			System.out.println("Error reading file '" + fileName + "'");
+		}
+	}
 }	
